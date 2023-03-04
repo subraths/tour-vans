e7807efd47d79473f1982281e7bcfab0ff4ede19
Subrath S <subrathitachi6@gmail.com>
2023-03-04 15:42:46 +0530

    fixed bug in home page(button url)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => (
       <h1>You got the travel plans, we got the travel vans</h1>
       <p>Add adventure to your life by joining the #vanlife movement.</p>
       <p>Rent the perfect van to make your perfect road trip.</p>
-      <Link to={"/van"} className="link-btn">
+      <Link to={"/vans"} className="link-btn">
         Find your van
       </Link>
     </section>
